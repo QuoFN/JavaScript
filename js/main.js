0607a52d327253f3b9d91bf92594be27b75aaf43
QuoFN <116729072+QuoFN@users.noreply.github.com>
2023-07-19 19:01:54 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+
+//       У С Л О В И Я         //
+
 // const age = 18
+
 // age >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18!');;
 
 // if (age >= 18) {
@@ -23,16 +27,50 @@
 // }
 
 
-for (let i = 1; i <= 10; i++) {
-    if (i > 4) {
-        console.log(`Пройдено ${i} кругов!`);
-    }
 
-    else if (i == 1) {
-        console.log(`Пройден ${i} круг!`);
-    }
+//        Ц И К Л Ы          //
 
-    else {
-        console.log(`Пройдено ${i} круга!`);
-    }
-}
+
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i > 4) {
+//         console.log(`Пройдено ${i} кругов!`);
+//     }
+
+//     else if (i == 1) {
+//         console.log(`Пройден ${i} круг!`);
+//     }
+
+//     else {
+//         console.log(`Пройдено ${i} круга!`);
+//     }
+// }
+
+
+
+// for (let i = 2; i <= 12; i = i + 2) {
+//     console.log(i);
+// }
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// } 
+
+// let i = 0
+
+// while (i < 12) {
+//     i = i + 2
+//     console.log(i);
+// }
+
+// let i = 11
+
+// while (i > 1) {
+//     i--
+//     console.log(i);
+// }
+
+
+//       М А С С И В Ы         //
+
+
