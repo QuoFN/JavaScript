894aafd9e784124dd81ed2c0144d8b2533ea0c42
QuoFN <116729072+QuoFN@users.noreply.github.com>
2023-07-19 03:11:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,38 @@
+// const age = 18
+// age >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18!');;
+
+// if (ageAnton >= 18) {
+//     console.log('Вы совершеннолетний');
+// }
+// else {
+//     console.log('Вам нет 18!');
+// }
+
+// switch (age) {
+//     case 16:
+//         console.log('Вам нет 18!');
+//         break;
+//     case 17:
+//         console.log('Вам нет 18!')
+//         break;
+//     case 18:
+//         console.log('Вы совершеннолетний')
+//         break;
+//     default:
+//         console.log('Таких значений нет');
+// }
+
+
+for (let i = 1; i <= 10; i++) {
+    if (i > 4) {
+        console.log(`Пройдено ${i} кругов!`);
+    }
+
+    else if (i == 1) {
+        console.log(`Пройден ${i} круг!`);
+    }
+
+    else {
+        console.log(`Пройдено ${i} круга!`);
+    }
+}
